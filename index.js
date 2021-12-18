cf692321bb124082702df805ff872b3183b63676
faishal-developer <foysal.professional@gmail.com>
2021-12-18 15:40:44 +0600

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const ObjectID = require('mongodb').ObjectID;
+const fileUpload = require('express-fileupload')
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -10,11 +11,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json())
+app.use(fileUpload());
 
 //connecting to database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yaqej.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+console.log(process.env.DB_USER);
 //database.collection api calling here
 async function run() {
     try {
@@ -22,7 +24,7 @@ async function run() {
         const database = client.db("cox_resort");
         const rooms = database.collection("rooms");
         const orders = database.collection("orders");
-
+        console.log('connected with mongodb');
         //getting rooms by get method
         app.get('/rooms', async (req, res) => {
             const cursor = rooms.find({});
@@ -54,6 +56,11 @@ async function run() {
         //posting a new service 
         app.post('/rooms', async (req, res) => {
             let body = req.body
+            const image = req.files.img;
+            const imageData = image.data;
+            const encodeImage = imageData.toString('base64')
+            const imageBuffer = Buffer.from(encodeImage, 'base64')
+            body.img = imageBuffer;
             const result = await rooms.insertOne(body);
             res.json(result);
         })
@@ -63,11 +70,24 @@ async function run() {
             const result = await orders.insertOne(body);
             res.json(result);
         })
+        //updating a order
+        app.put('/orders/:itemId', async (req, res) => {
+            const id = req.params.itemId;
+            const filter = { _id: ObjectID(id) };
+            console.log('working');
+            const updateDoc = {
+                $set: {
+                    status: 'okay'
+                },
+            };
+            const result = await orders.updateOne(filter, updateDoc);
+            res.json(result);
+
+        })
 
         //deleting a order
         app.delete('/orders/:itemId', async (req, res) => {
             const id = req.params.itemId;
-            console.log('working', id);
             const query = { _id: ObjectID(id) };
             const result = await orders.deleteOne(query);
             res.json(result)
